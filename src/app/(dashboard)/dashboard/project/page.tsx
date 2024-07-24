ad05b78c01726af3034db99cdba4974031bf06b8
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-07-24 08:28:21 +0000

    Implemented server actions to get data from rust api

--- src/app/(dashboard)/dashboard/project/page.tsx
+++ src/app/(dashboard)/dashboard/project/page.tsx
@@ -1,16 +1,28 @@
+"use client"
+import { getFiles } from '@/actions';
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import { UserClient } from '@/components/tables/user-tables/client';
 import { users } from '@/constants/data';
+import { useEffect, useState } from 'react';
 
 const breadcrumbItems = [
   { title: 'Dashboard', link: '/dashboard' },
   { title: 'User', link: '/dashboard/user' }
 ];
 export default function page() {
+  const [data,setData] = useState()
+  useEffect(()=>{
+    getFiles().then((res)=>{
+      if (!res.success) {
+        alert("Something went wrong")
+      }
+      setData(res.body)
+    })
+  })
   return (
     <>
       <div className="flex-1 space-y-4  p-4 pt-6 md:p-8">
-
+          {JSON.stringify(data)}
         <Breadcrumbs items={breadcrumbItems} />
         <UserClient data={users} />
       </div>
